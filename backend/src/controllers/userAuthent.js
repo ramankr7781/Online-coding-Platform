8e465229b7e4bd7874f3031ac0d6fb23d82631fb
Raman Kumar <ramankumar825285@gmail.com>
2026-06-25 14:21:07 +0530

    fix: set sameSite and secure attributes for cookies to allow cross-site requests in production

--- backend/src/controllers/userAuthent.js
+++ backend/src/controllers/userAuthent.js
@@ -27,7 +27,12 @@ const register = async (req,res)=>{
         role:user.role,
     }
     
-     res.cookie('token',token,{maxAge: 60*60*1000});
+     res.cookie('token',token,{
+        maxAge: 60*60*1000,
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none'
+     });
      res.status(201).json({
         user:reply,
         message:"Loggin Successfully"
@@ -67,7 +72,12 @@ const login = async (req,res)=>{
         }
 
         const token =  jwt.sign({_id:user._id , emailId:emailId, role:user.role},process.env.JWT_KEY,{expiresIn: 60*60});
-        res.cookie('token',token,{maxAge: 60*60*1000});
+        res.cookie('token',token,{
+            maxAge: 60*60*1000,
+            httpOnly: true,
+            secure: true,
+            sameSite: 'none'
+        });
         res.status(201).json({
             user:reply,
             message:"Loggin Successfully"
@@ -93,7 +103,12 @@ const logout = async(req,res)=>{
     //    Token add kar dung Redis ke blockList
     //    Cookies ko clear kar dena.....
 
-    res.cookie("token",null,{expires: new Date(Date.now())});
+    res.cookie("token",null,{
+        expires: new Date(Date.now()),
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none'
+    });
     res.send("Logged Out Succesfully");
 
     }
@@ -117,7 +132,12 @@ const adminRegister = async(req,res)=>{
     
      const user =  await User.create(req.body);
      const token =  jwt.sign({_id:user._id , emailId:emailId, role:user.role},process.env.JWT_KEY,{expiresIn: 60*60});
-     res.cookie('token',token,{maxAge: 60*60*1000});
+     res.cookie('token',token,{
+        maxAge: 60*60*1000,
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none'
+     });
      res.status(201).send("User Registered Successfully");
     }
     catch(err){
